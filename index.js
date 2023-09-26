f46a990c8e8bb63361cb2c3120acc665dd6e42d1
Koraxtu <bukason2013@gmail.com>
2023-09-26 03:26:45 -0400

    Added Dockerfile

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const redis = require('redis');
 
 const app = express();
 const client = redis.createClient();
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
