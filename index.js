93e23b5529a4bce70acd110f826e2133898b8877
Koraxtu <bukason2013@gmail.com>
2023-09-26 03:14:42 -0400

    Counting visits

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const redis = require('redis');
 
 const app = express();
 const client = redis.createClient();
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Listening on port 8081');
+});
